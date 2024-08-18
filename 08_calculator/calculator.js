6afe537993a5e531801efc18558f8127e028dc29
cheek19833435 <cheekiboi@gmail.com>
2024-08-18 16:09:30 +1000

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr
+    .reduce((total,currentItem) => total+=currentItem,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr
+    .reduce((total,currentItem) => total*=currentItem)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = num
+  if (num==0){
+    return 1
+  } else if (num==1) {
+    return 1
+  } else {
+      while (num>0){
+        num-=1
+        if (num==1){
+          return total
+        }
+        total*=num
+        console.log(total)
+      }
+      return total
+  }
 };
 
 // Do not edit below this line
